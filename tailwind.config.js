0f63c0b4e67b43ea2fbe9507a77edca4418e9cd1
mkevinrinaldi <mkevinrinaldi@gmail.com>
2024-07-18 13:10:50 +0700

    perbaikan

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    colors: {
+      warna2: "#1A3636",
+      white: "#fff",
+      warna1: "#D6BD98",
+      warna3: "#40534C",
+      warna4: "#677D6A",
+      warnahijau: "#508D4E",
+    },
+  },
+};
